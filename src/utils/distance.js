605149143791c7c7c9a089b098abba8a372fa297
regorxxx <regorxxx@protonmail.com>
2024-02-04 19:38:28 +0100

    Replace OKLCH recognition with RegExp for future modes. Replace weights argument with null (as other methods) and fill if needed.
    Signed-off-by: regorxxx <regorxxx@protonmail.com>

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,19 +1,26 @@
 const Color = require('../Color');
 
 // simple Euclidean distance
-module.exports = function(a, b, mode='lab', weights = [1, 1, 1, 1]) {//need 4 color weights for CMYK color space
-
+module.exports = function(a, b, mode='lab', weights=null) {
+    if (weights) { //needs 4 color weights for CMYK color space
+        const inputWeights = weights;
+        weights = new Array(4).fill(1);
+        inputWeights.slice(0,4).forEach((w, i) => {
+            if (!isNaN(w)) {weights[i] = w;}
+        });
+    } else {weights = Array(4).fill(1);}
     // Delta E (CIE 1976)
     // see http://www.brucelindbloom.com/index.html?Equations.html
     a = new Color(a);
     b = new Color(b);
     const l1 = a.get(mode);
     const l2 = b.get(mode);
+    const intMode = mode.replace(/(^ok)/gi, ''); // OKLCH behaves like LCH
     let sum_sq = 0;
     let d;
     for (let i in l1) {
-        if (mode.replace('ok', '').charAt[i] === 'h') {
-            var hueDifference = (l1[i] || 0) - (l2[i] || 0);
+        if (intMode.charAt[i] === 'h') {
+            const hueDifference = (l1[i] || 0) - (l2[i] || 0);
             d = Math.abs(hueDifference + 180) % 360 - 180;
         }
         else {
