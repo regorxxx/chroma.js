db11f9d7b7a22fe1ee2e1ced510e7585a4ecf119
regorxxx <regorxxx@protonmail.com>
2024-02-05 22:46:22 +0100

    * added LAB, LCH, OKLCH and OKLAB CSS support in "chroma()". i.e. CSS expressions are also interpreted as colors `chroma("lch(48.25% 30.07% 196.38)")`
    Signed-off-by: regorxxx <regorxxx@protonmail.com>

--- src/io/css/css2rgb.js
+++ src/io/css/css2rgb.js
@@ -1,4 +1,8 @@
 const hsl2rgb = require('../hsl/hsl2rgb');
+const lab2rgb = require('../lab/lab2rgb');
+const lch2rgb = require('../lch/lch2rgb');
+const oklab2rgb = require('../oklab/oklab2rgb');
+const oklch2rgb = require('../oklch/oklch2rgb');
 const input = require('../input');
 
 const RE_RGB = /^rgb\(\s*(-?\d+),\s*(-?\d+)\s*,\s*(-?\d+)\s*\)$/;
@@ -7,6 +11,10 @@ const RE_RGB_PCT = /^rgb\(\s*(-?\d+(?:\.\d+)?)%,\s*(-?\d+(?:\.\d+)?)%\s*,\s*(-?\
 const RE_RGBA_PCT = /^rgba\(\s*(-?\d+(?:\.\d+)?)%,\s*(-?\d+(?:\.\d+)?)%\s*,\s*(-?\d+(?:\.\d+)?)%\s*,\s*([01]|[01]?\.\d+)\)$/;
 const RE_HSL = /^hsl\(\s*(-?\d+(?:\.\d+)?),\s*(-?\d+(?:\.\d+)?)%\s*,\s*(-?\d+(?:\.\d+)?)%\s*\)$/;
 const RE_HSLA = /^hsla\(\s*(-?\d+(?:\.\d+)?),\s*(-?\d+(?:\.\d+)?)%\s*,\s*(-?\d+(?:\.\d+)?)%\s*,\s*([01]|[01]?\.\d+)\)$/;
+const RE_LABA = /^lab\(\s*(\d+(?:\.\d+)?)%?\s* (-?\d+(?:\.\d+)?)%?\s* \s*(-?\d+(?:\.\d+)?)%?\s*(?:\/\s*([01]|[01]?\.\d+))?\)$/;
+const RE_LCHA = /^lch\(\s*(\d+(?:\.\d+)?)%?\s* (\d+(?:\.\d+)?)%?\s* \s*(-?\d+(?:\.\d+)?)\s*(?:\/\s*([01]|[01]?\.\d+))?\)$/;
+const RE_OKLABA = /^oklab\(\s*(\d+(?:\.\d+)?%?)\s* (-?\d+(?:\.\d+)?%?)\s* \s*(-?\d+(?:\.\d+)?%?)\s*(?:\/\s*([01]|[01]?\.\d+))?\)$/;
+const RE_OKLCHA = /^oklch\(\s*(\d+(?:\.\d+)?%?)\s* (\d+(?:\.\d+)?%?)\s* \s*(-?\d+(?:\.\d+)?)\s*(?:\/\s*([01]|[01]?\.\d+))?\)$/;
 
 const {round} = Math;
 
@@ -26,7 +34,7 @@ const css2rgb = (css) => {
     if ((m = css.match(RE_RGB))) {
         const rgb = m.slice(1,4);
         for (let i=0; i<3; i++) {
-            rgb[i] = +rgb[i];
+            rgb[i] = Number(rgb[i]);
         }
         rgb[3] = 1;  // default alpha
         return rgb;
@@ -36,7 +44,7 @@ const css2rgb = (css) => {
     if ((m = css.match(RE_RGBA))) {
         const rgb = m.slice(1,5);
         for (let i=0; i<4; i++) {
-            rgb[i] = +rgb[i]
+            rgb[i] = Number(rgb[i]);
         }
         return rgb;
     }
@@ -45,7 +53,7 @@ const css2rgb = (css) => {
     if ((m = css.match(RE_RGB_PCT))) {
         const rgb = m.slice(1,4);
         for (let i=0; i<3; i++) {
-            rgb[i] = round(rgb[i] * 2.55);
+            rgb[i] = round(Number(rgb[i]) * 2.55);
         }
         rgb[3] = 1;  // default alpha
         return rgb;
@@ -55,7 +63,7 @@ const css2rgb = (css) => {
     if ((m = css.match(RE_RGBA_PCT))) {
         const rgb = m.slice(1,5);
         for (let i=0; i<3; i++) {
-            rgb[i] = round(rgb[i] * 2.55);
+            rgb[i] = round(Number(rgb[i]) * 2.55);
         }
         rgb[3] = +rgb[3];
         return rgb;
@@ -64,6 +72,7 @@ const css2rgb = (css) => {
     // hsl(0,100%,50%)
     if ((m = css.match(RE_HSL))) {
         const hsl = m.slice(1,4);
+        for (let i = 0; i <= 2; i++) {hsl[i] = Number(hsl[i]);}
         hsl[1] *= 0.01;
         hsl[2] *= 0.01;
         const rgb = hsl2rgb(hsl);
@@ -74,9 +83,52 @@ const css2rgb = (css) => {
     // hsla(0,100%,50%,0.5)
     if ((m = css.match(RE_HSLA))) {
         const hsl = m.slice(1,4);
+        for (let i = 0; i <= 2; i++) {hsl[i] = Number(hsl[i]);}
         hsl[1] *= 0.01;
         hsl[2] *= 0.01;
         const rgb = hsl2rgb(hsl);
+        rgb[3] = +m[4]  // default alpha = 1
+        return rgb;
+    }
+    
+    // lab(48.25% -28.85% -8.48% / 1)
+    if ((m = css.match(RE_LABA))) {
+        const lab = m.slice(1,4);
+        for (let i = 0; i <= 2; i++) {lab[i] = Number(lab[i]);}
+        const rgb = lab2rgb(lab);
+        rgb[3] = +m[4]  // default alpha = 1
+        return rgb;
+    }
+    
+    // lch(54.31% 9.27% 194.77 / 1)
+    if ((m = css.match(RE_LCHA))) {
+        const lch = m.slice(1,4);
+        for (let i = 0; i <= 2; i++) {lch[i] = Number(lch[i]);}
+        const rgb = lch2rgb(lch);
+        rgb[3] = +m[4]  // default alpha = 1
+        return rgb;
+    }
+    
+    // oklab(54.31% -8.96% -2.36% / 1)
+    if ((m = css.match(RE_OKLABA))) {
+        const lab = m.slice(1,4);
+        for (let i = 0; i <= 2; i++) {
+            if (lab[i].endsWith('%')) {lab[i] = Number(lab[i].replace('%', '')) * 0.01;}
+            else {lab[i] = Number(lab[i]);}
+        }
+        const rgb = oklab2rgb(lab);
+        rgb[3] = +m[4]  // default alpha = 1
+        return rgb;
+    }
+    
+    // oklch(54.31% 9.27% 194.77 / 1)
+    if ((m = css.match(RE_OKLCHA))) {
+        const lch = m.slice(1,4);
+        for (let i = 0; i <= 1; i++) {
+            if (lch[i].endsWith('%')) {lch[i] = Number(lch[i].replace('%', '')) * 0.01;}
+            else {lab[i] = Number(lab[i]);}
+        }
+        const rgb = oklch2rgb(lch);
         rgb[3] = +m[4]  // default alpha = 1
         return rgb;
     }
@@ -88,7 +140,11 @@ css2rgb.test = (s) => {
         RE_RGB_PCT.test(s) ||
         RE_RGBA_PCT.test(s) ||
         RE_HSL.test(s) ||
-        RE_HSLA.test(s);
+        RE_HSLA.test(s) ||
+        RE_LABA.test(s) ||
+        RE_LCHA.test(s) ||
+        RE_OKLABA.test(s) ||
+        RE_OKLCHA.test(s);
 }
 
 module.exports = css2rgb;
